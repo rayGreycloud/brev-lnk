33a2cc7db6dcd20489d94c6c495f62a9317b0c21
rayGreycloud <rayGreycloud@gmail.com>
2017-03-12 07:43:08 -0700

    Add basic Link

--- client/main.js
+++ client/main.js
@@ -2,8 +2,9 @@ import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Link from './../imports/ui/Link';
 import Signup from './../imports/ui/Signup';
 
 Meteor.startup(() => {
-  ReactDOM.render(<Signup/>, document.getElementById('app'));
+  ReactDOM.render(<Link/>, document.getElementById('app'));
 });
---
+++ imports/ui/Link.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Link extends Component {
+  render() {
+    return (
+      <div>Link component here</div>
+    );
+  }
+}
+
+export default Link;
