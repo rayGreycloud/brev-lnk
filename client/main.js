7f39be02576ff9fd969aad07207ce16e7494adc8
rayGreycloud <rayGreycloud@gmail.com>
2017-03-13 06:58:32 -0700

    Add router and routes to client server

--- client/main.js
+++ client/main.js
@@ -1,10 +1,19 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import Link from './../imports/ui/Link';
 import Signup from './../imports/ui/Signup';
 
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/signup" component={Signup}/>
+    <Route path="/links" component={Link}/>
+
+  </Router>
+);
+
 Meteor.startup(() => {
-  ReactDOM.render(<Link/>, document.getElementById('app'));
+  ReactDOM.render(routes, document.getElementById('app'));
 });
