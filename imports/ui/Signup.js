18d3d024094d588a9de2c464a2c220fbc71bf773
rayGreycloud <rayGreycloud@gmail.com>
2017-03-13 08:36:57 -0700

    Add form to Signup

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,11 +2,36 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 class Signup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: ''
+    };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.setState({
+      error: 'Ding ding ding'
+    });
+  }
+
   render() {
     return (
       <div>
-        <h2>Sign Up Page</h2>
-        <div>Signup form goes here.</div>
+        <h2>BrevLnk Sign Up</h2>
+
+        {this.state.error ? <p>{this.state.error}</p> : undefined }
+
+        <div>
+          <form onSubmit={this.onSubmit.bind(this)}>
+            <input type="email" name="email" placeholder="Email"/>
+            <input type="password" name="password" placeholder="Password"/>
+            <button>Create Account</button>
+          </form>
+        </div>
         <Link to="/">Have an account? Sign in here</Link>
       </div>
     );
