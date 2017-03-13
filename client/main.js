73bf99df504cc156b25a567478ce94729d70c116
rayGreycloud <rayGreycloud@gmail.com>
2017-03-13 07:08:03 -0700

    Add login and 404 routes to router

--- client/main.js
+++ client/main.js
@@ -4,13 +4,16 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import Link from './../imports/ui/Link';
+import Login from './../imports/ui/Login';
+import NotFound from './../imports/ui/NotFound';
 import Signup from './../imports/ui/Signup';
 
 const routes = (
   <Router history={browserHistory}>
-    <Route path="/signup" component={Signup}/>
-    <Route path="/links" component={Link}/>
-
+    <Route path="/" component={Login} />
+    <Route path="/signup" component={Signup} />
+    <Route path="/links" component={Link} />
+    <Route path="*" component={NotFound} />
   </Router>
 );
 
