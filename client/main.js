27a08fa5a2e499c228cb3f69584a399bbf5ad94a
rayGreycloud <rayGreycloud@gmail.com>
2017-03-17 06:49:05 -0700

    Add links collection and connect to client

--- client/main.js
+++ client/main.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import {Tracker} from 'meteor/tracker';
 
 import { onAuthChange, routes } from './../imports/routes/routes';
+import { Links } from './../imports/api/links';
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
   onAuthChange(isAuthenticated);
+});
+
+Tracker.autorun(() => {
+  const linkList = Links.find().fetch();
+  console.log(linkList);
 });
 
 Meteor.startup(() => {
