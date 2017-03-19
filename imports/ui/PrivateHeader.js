cf78951810f0eda34772ced1665fada277d73544
rayGreycloud <rayGreycloud@gmail.com>
2017-03-19 05:55:19 -0700

    Scaffold PrivateHeader component

---
+++ imports/ui/PrivateHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+
+class PrivateHeader extends Component {
+  render() {
+    return (
+
+    );
+  }
+};
+
+export default PrivateHeader;
