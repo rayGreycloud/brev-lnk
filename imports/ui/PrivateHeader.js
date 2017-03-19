b9eba72d5e1bf469a1ac48884d0ab5952b1cecc5
rayGreycloud <rayGreycloud@gmail.com>
2017-03-19 06:57:12 -0700

    Refactor PrivateHeader into stateless functional component

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -2,20 +2,15 @@ import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
-class PrivateHeader extends Component {
-  onLogout() {
-    Accounts.logout();
-  }
-  render() {
-    return (
-      <div>
-        <h2>{this.props.title}</h2>
-        <button onClick={this.onLogout.bind(this)}>
-          Logout
-        </button>
-      </div>
-    );
-  }
+const PrivateHeader = (props) => {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <button onClick={() => Accounts.logout()}>
+        Logout
+      </button>
+    </div>
+  );
 };
 
 PrivateHeader.propTypes = {
