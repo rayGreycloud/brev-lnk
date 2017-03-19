1d4297e3d9b8714e36768690d5c9750a799fc2a9
rayGreycloud <rayGreycloud@gmail.com>
2017-03-19 06:06:19 -0700

    Scaffold AddLink component

---
+++ imports/ui/AddLink.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+
+class AddLink extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default AddLink;
