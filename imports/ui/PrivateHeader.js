7c442f0041a1cdba9384fbfa5c284ff853093c9a
rayGreycloud <rayGreycloud@gmail.com>
2017-03-19 06:13:27 -0700

    Move header jsx and method to PrivateHeader component

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -3,9 +3,22 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
 class PrivateHeader extends Component {
+  constructor(props) {
+    super(props);
+
+
+  }
+  onLogout() {
+    Accounts.logout();
+  }
   render() {
     return (
-
+      <div>
+        <h2>Your Links</h2>
+        <button onClick={this.onLogout.bind(this)}>
+          Logout
+        </button>
+      </div>
     );
   }
 };
