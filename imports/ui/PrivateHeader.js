1353e586e8542df449ff85020de4358fad922dc0
rayGreycloud <rayGreycloud@gmail.com>
2017-03-19 06:33:44 -0700

    Add propType to PrivateHeader and remove unneeded constructor

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -3,24 +3,23 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
 class PrivateHeader extends Component {
-  constructor(props) {
-    super(props);
-
-
-  }
   onLogout() {
     Accounts.logout();
   }
   render() {
     return (
       <div>
-        <h2>Your Links</h2>
+        <h2>{this.props.title}</h2>
         <button onClick={this.onLogout.bind(this)}>
           Logout
         </button>
       </div>
     );
   }
+};
+
+PrivateHeader.propTypes = {
+  title: React.PropTypes.string.isRequired
 };
 
 export default PrivateHeader;
