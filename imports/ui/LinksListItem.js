b5ee90d84a16c2db0267a67e4e5e440fc59eaa78
rayGreycloud <rayGreycloud@gmail.com>
2017-03-20 07:00:00 -0700

    Add clipboard package in LinksListItem

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import Clipboard from 'clipboard';
 
 class LinksListItem extends Component {
   constructor(props) {
     super(props);
+  }
+  componentDidMount() {
+    this.clipboard = new Clipboard(this.refs.copy);
+
+    this.clipboard.on('success', () => {
+      alert('Copy Successful');
+    }).on('error', () => {
+      alert('Unable to copy - ERROR');
+    });
+  }
+  componentWillUnmount() {
+    this.clipboard.destroy();
   }
   render() {
     return (
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
-        <p>visits</p>
+        <p>0 visits</p>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl}>COPY</button>
       </div>
     );
   }
