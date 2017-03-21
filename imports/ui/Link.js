2d29ac87b5e85aa6328752759331f886bb9aa8a2
rayGreycloud <rayGreycloud@gmail.com>
2017-03-21 06:24:27 -0700

    Add LinksListFilters component with checkbox to toggle visibility of hidden links

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -4,11 +4,13 @@ import { Links } from './../api/links';
 import PrivateHeader from './PrivateHeader';
 import LinksList from './LinksList';
 import AddLink from './AddLink';
+import LinksListFilters from './LinksListFilters';
 
 const Link = () => {
   return (
     <div>
       <PrivateHeader title="Your Links" />
+      <LinksListFilters />
       <LinksList />
       <AddLink />
     </div>
