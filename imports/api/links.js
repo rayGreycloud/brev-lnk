bf15dc0529bfb16a5deaf7c612c676ad68c48b3b
rayGreycloud <rayGreycloud@gmail.com>
2017-03-21 08:52:25 -0700

    Add links.trackVisit method for visitedCount and lastVisitedAt

--- imports/api/links.js
+++ imports/api/links.js
@@ -31,7 +31,9 @@ Meteor.methods({
       _id: shortid.generate(),
       url,
       userId: this.userId,
-      visible: true
+      visible: true,
+      visitedCount: 0,
+      lastVisitedAt: null
     });
   },
   'links.setVisibility'(_id, visible) {
@@ -40,7 +42,7 @@ Meteor.methods({
     }
 
     new SimpleSchema({
-      _id : {
+      _id: {
         type: String,
         min: 1
       },
@@ -54,6 +56,23 @@ Meteor.methods({
       userId: this.userId
     }, {
       $set: { visible }
+    });
+  },
+  'links.trackVisit'(_id) {
+    new SimpleSchema({
+      _id: {
+        type: String,
+        min: 1
+      }
+    }).validate({ _id });
+
+    Links.update({ _id }, {
+      $set: {
+        lastVisitedAt: new Date().getTime()
+      },
+      $inc: {
+        visitedCount: 1
+      }
     });
   }
 });
