86e6c7ae6831a54c866c9eea6b8e04579cd5f010
rayGreycloud <rayGreycloud@gmail.com>
2017-03-21 07:15:55 -0700

    Convert AddLink url input to controlled

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -2,22 +2,42 @@ import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 
 class AddLink extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      url: ''
+    };
+  }
   onSubmit(e) {
-    const url = this.refs.url.value.trim();
+    const { url } = this.state;
 
     e.preventDefault();
 
     if (url) {
-      Meteor.call('links.insert', url);
-      this.refs.url.value = '';
+      Meteor.call('links.insert', url, (err, res) => {
+        if (!err) {
+          this.setState({ url: '' });
+        }
+      });
     }
+  }
+  onChange(e) {
+    this.setState({
+      url: e.target.value
+    });
   }
   render() {
     return (
       <div>
         <p>Add Link</p>
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="text" ref="url" placeholder="URL" />
+          <input
+            type="text"
+            placeholder="URL"
+            value={this.state.url}
+            onChange={this.onChange.bind(this)}
+          />
           <button>Add Link</button>
         </form>
       </div>
