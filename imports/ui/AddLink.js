f0dee72c73c8163c000452737fb1450f52366d1b
rayGreycloud <rayGreycloud@gmail.com>
2017-03-22 12:56:02 -0700

    Add input focus on modal open and close modal on background click

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -19,7 +19,7 @@ class AddLink extends Component {
 
     Meteor.call('links.insert', url, (err, res) => {
       if (!err) {
-        this.setState({ url: '', isOpen: false, error: '' });
+        this.handleModalClose();
       } else {
         this.setState({ error: err.reason });
       }
@@ -30,23 +30,36 @@ class AddLink extends Component {
       url: e.target.value
     });
   }
+  handleModalClose() {
+    this.setState({
+      isOpen: false,
+      url: '',
+      error: ''
+    });
+  }
   render() {
     return (
       <div>
         <button onClick={() => this.setState({ isOpen: true})}>+ Add Link</button>
-        <Modal isOpen={this.state.isOpen} contentLabel="Add Link">
+        <Modal
+          isOpen={this.state.isOpen}
+          contentLabel="Add Link"
+          onAfterOpen={() => this.refs.url.focus()}
+          onRequestClose={this.handleModalClose.bind(this)}
+          >
           <h2>Add Link</h2>
           {this.state.error ? <p>{this.state.error}</p> : undefined }
           <form onSubmit={this.onSubmit.bind(this)}>
             <input
               type="text"
               placeholder="URL"
+              ref="url"
               value={this.state.url}
               onChange={this.onChange.bind(this)}
             />
             <button>Add Link</button>
           </form>
-          <button onClick={() => this.setState({ isOpen: false, url: '', error: ''})}>Cancel</button>
+          <button onClick={this.handleModalClose.bind(this)}>Cancel</button>
         </Modal>
       </div>
     );
