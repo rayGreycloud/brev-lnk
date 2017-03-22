4cc00c7a0e3caa4bb5e0f284dfab831129951dec
rayGreycloud <rayGreycloud@gmail.com>
2017-03-22 12:59:34 -0700

    Add link tag for links

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -43,6 +43,9 @@ class LinksListItem extends Component {
         <p>{this.props.shortUrl}</p>
         <p>{this.props.visible.toString()}</p>
         {this.renderStats()}
+        <a href={this.props.shortUrl} target="_blank">
+          Visit
+        </a>
         <button ref="copy" data-clipboard-text={this.props.shortUrl}>{this.state.justCopied ? 'Copied' : 'Copy'}</button>
         <button onClick={() => {
           Meteor.call('links.setVisibility', this.props._id, !this.props.visible)
