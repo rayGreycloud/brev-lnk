abde581c67688974a6777da2d1417a93d169a85f
rayGreycloud <rayGreycloud@gmail.com>
2017-03-22 06:15:51 -0700

    Add moment and renderStats method with timestamp to LinksListItem

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import Clipboard from 'clipboard';
+import Moment from 'moment';
 
 class LinksListItem extends Component {
   constructor(props) {
@@ -24,13 +25,24 @@ class LinksListItem extends Component {
   componentWillUnmount() {
     this.clipboard.destroy();
   }
+  renderStats() {
+    const visitMessage = this.props.visitedCount === 1 ? 'visit' : 'visits';
+    let visitedMessage = null;
+
+    if (typeof this.props.lastVisitedAt === 'number') {
+      const fromNow = Moment(this.props.lastVisitedAt).fromNow();
+      visitedMessage = `(visited ${fromNow})`
+    }
+
+    return <p>{this.props.visitedCount} {visitMessage} {visitedMessage}</p>
+  }
   render() {
     return (
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
         <p>{this.props.visible.toString()}</p>
-        <p>{this.props.visitedCount} visits - last visited: {this.props.lastVisitedAt}</p>
+        {this.renderStats()}
         <button ref="copy" data-clipboard-text={this.props.shortUrl}>{this.state.justCopied ? 'Copied' : 'Copy'}</button>
         <button onClick={() => {
           Meteor.call('links.setVisibility', this.props._id, !this.props.visible)
